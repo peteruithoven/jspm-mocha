71ef74973df45ea524547e173387fccb0ba78b05
peteruithoven <peter@peteruithoven.nl>
2015-08-31 13:48:46 +0200

    first

---
+++ config.js
@@ -0,0 +1,36 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.23",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "core-js": "npm:core-js@1.1.3",
+    "mocha": "npm:mocha@2.3.0",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.8.20": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.1.3": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:mocha@2.3.0": {
+      "css": "github:systemjs/plugin-css@0.1.15"
+    }
+  }
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+import Mocha, { describe, it } from 'mocha';
+console.log('Mocha: ', Mocha); // > mocha object
+console.log('describe: ', describe); // > undefined
+console.log('it: ', it); // > undefined
